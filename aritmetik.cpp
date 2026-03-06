5cdfe89a5de34ebbad603415c0b7bfe2bcb2cce9
Yesim <120514583+yesimyigitbasi@users.noreply.github.com>
2026-03-06 15:12:32 +0300

    Lab 02 deneme

---
+++ aritmetik.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int count = 10;
+    int num1 = 15;
+    int num2 =8;
+
+    /*
+    cout << "Toplam: " << (num1 + num2) <<endl;
+    cout << "Çıkarma: " << (num1 - num2) <<endl;
+    cout << "Çarpma: " << (num1 * num2) <<endl;
+    cout << "Bölüm: " << (num1 / num2) <<endl;
+    cout << "Kalan: " << (num1 % num2) <<endl;
+    */
+
+    cout << "++count " << ++count << endl;
+    cout << "count++ " << count++ << endl;
+    cout << "son " << count << endl;
+
+
+    int count1 = 5;
+    cout << "--count1 " << --count1 << endl;
+    cout << "count1-- " << count1-- << endl;
+    cout << "son " << count1;
+
+}
