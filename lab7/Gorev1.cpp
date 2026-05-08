e2586afbdf1c2b77abdff770b89baff65b1585b9
yesimyigitbasi <120514583+yesimyigitbasi@users.noreply.github.com>
2026-05-08 14:21:00 +0300

    gorev 1

--- lab7/Gorev1.cpp
+++ lab7/Gorev1.cpp
@@ -2,9 +2,33 @@
 using namespace std;
 
 class Dikdortgen {
+public:            
+    float genislik;
+    float yukseklik;
 
+    float alan() {
+        return genislik * yukseklik;  
+    }
+
+    float cevre() {
+        return 2 * (genislik + yukseklik);
+    }
+
+    void yazBilgi() {
+        cout << "Boyut  : " << genislik << " x " << yukseklik << endl;
+        cout << "Alan   : " << alan()   << " cm2" << endl;  
+        cout << "Cevre  : " << cevre()  << " cm"  << endl; 
+    }
 };
 
 int main() {
-    
+    Dikdortgen d1, d2, d3;
+    d1.genislik = 5.0;  d1.yukseklik = 3.0;
+    d2.genislik = 8.5;  d2.yukseklik = 4.2;
+    d3.genislik = 10.0; d3.yukseklik = 2.5;
+
+    cout << "--- Dikdortgen 1 ---" << endl; d1.yazBilgi();
+    cout << "--- Dikdortgen 2 ---" << endl; d2.yazBilgi();
+    cout << "--- Dikdortgen 3 ---" << endl; d3.yazBilgi();
+    return 0;
 }
